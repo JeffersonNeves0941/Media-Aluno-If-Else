b1851252ee050c5b454c615eb1b9a1e53631df87
JEFFERSON VICTOR NEVES DA SILVA <25204074@cfp127aluno.local>
2025-09-22 15:17:09 -0300

    first commit

--- src/mediaAluno.java
+++ src/mediaAluno.java
@@ -1,4 +1,50 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class MediaAluno {
-}
+public class mediaAluno {
+
+    String nomeDoAluno;
+    double nota1;
+    double nota2;
+    double nota3;
+    double nota4;
+    double mediaFinal;
+    double somaDasNotas;
+
+    public void coletarDados() {
+        Scanner leitor = new Scanner(System.in);
+        System.out.printf("Digite o nome do Aluno");
+        nomeDoAluno = leitor.nextLine();
+
+        System.out.printf("Digite a primeira nota do aluno: ");
+        nota1 = leitor.nextDouble();
+
+        System.out.printf("Digite a segunda nota do aluno: ");
+        nota2 = leitor.nextDouble();
+
+        System.out.printf("Digite a terceira nota do aluno: ");
+        nota3 = leitor.nextDouble();
+
+        System.out.printf("Digite a quarta nota do aluno: ");
+        nota4 = leitor.nextDouble();
+
+        calcularMedia();
+        exibirResultado();
+    }
+
+    public void calcularMedia(){
+     somaDasNotas = nota1 + nota2 + nota3 + nota4;
+     mediaFinal = somaDasNotas / 4;
+    }
+
+    public void exibirResultado () {
+
+        System.out.println("A media do aluno é; " + mediaFinal);
+
+        if (mediaFinal >= 5) {
+            System.out.println("O aluno " + nomeDoAluno + " foi " + " Aprovado");
+        } else {
+            System.out.println("O aluno " + nomeDoAluno + " foi " + " Reprovado");
+        }
+
+    }
+    }
--- src/mediaAlunoApp.java
+++ src/mediaAlunoApp.java
@@ -1,8 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+
+public class mediaAlunoApp {
     public static void main(String[] args) {
         System.out.println("Iniciando");
+        mediaAluno iniciar = new mediaAluno();
+        iniciar.coletarDados();
+
+
 
 
     }
